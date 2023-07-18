9d81e37a6c2338d4399d92e8404e40164c68db1f
ahmedhasan241 <92892508+ahmedhasan241@users.noreply.github.com>
2023-07-18 06:52:52 +0300

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HomeHeader />
+    <HomePosters />
+    <HomeBestSeller />
+    <HomeInfo />
+  </div>
+</template>
+
+<script setup>
+import { useCartStore } from '~/stores/cart';
+useHead({
+  titleTemplate: "Bouncer - Home",
+});
+
+let cart = useCartStore();
+if(process.client) {
+  cart.getItemsToCart()
+}
+</script>
+
+<style>
+html[dir="en"] {
+  direction: ltr;
+}
+
+html[dir="ar"] {
+  direction: rtl;
+}
+</style>
